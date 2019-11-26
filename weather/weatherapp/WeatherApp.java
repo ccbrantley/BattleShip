d92da39640284ed4f1f3aa251f7c6732f051028b
ccbrantley <ccbrantley@gtcc.edu>
2019-11-26 02:38:25 -0500

    Update WeatherApp.java

--- weather/weatherapp/WeatherApp.java
+++ weather/weatherapp/WeatherApp.java
@@ -1,22 +1,23 @@
 package battleship.weather.weatherapp;
-/**
- * This class is for demonstrating the use of a weather api.
- *
- * @author Andrew Braswell Last Updated: 11/25/2019
- */
-import java.text.DecimalFormat;
 
+/* @author Area 51 Block Party:
+ * Andrew Braswell
+ * Last Updated: 11/25/2019
+ * This class is for demonstrating the use of a weather api.
+ */
+
+import java.text.DecimalFormat;
 import battleship.weather.models.Weather;
 
-
 public class WeatherApp {
+
     public static void main(String[] args) {
         DecimalFormat f = new DecimalFormat("#.##");
-
         for (Weather w : Weather.loadWeathersForAllLocations()) {
             System.out.println("The temperature in " + w.getLocationName() + " is " + w.getTemperature() + " F.");
             System.out.println("Windspeed: " + w.getWindSpeed() + ".  Wind direction: " + w.getWindDirection());
             System.out.println("The wind is blowing " + f.format(w.getXWindSpeed()) + " mph from the west and " + f.format(w.getYWindSpeed()) + " mph from the north. \n");
         }
     }
+
 }
