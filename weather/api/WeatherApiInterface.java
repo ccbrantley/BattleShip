e45bc3343837d9aa0d2423a363e9018d0b8bcbaa
ccbrantley <ccbrantley@gtcc.edu>
2019-11-26 21:55:02 -0500

    Update WeatherApiInterface.java

--- weather/api/WeatherApiInterface.java
+++ weather/api/WeatherApiInterface.java
@@ -1,22 +1,23 @@
 package battleship.weather.api;
-/**
+
+/* @author Area 51 Block Party:
+ * Andrew Braswell
+ * Last Updated: 11/26/2019
  * This interface defines the methods that need to be implemented for any
  * valid weather API translator class.
- *
- * @author Andrew Braswell Last Updated: 11/26/2019
  */
+
 import battleship.weather.util.Location;
 
 public interface WeatherApiInterface {
+
     /**
-     * Fetches weather information from the API
+     * Fetches weather information from the API.
      * @param _location A location object.
      */
     public void fetchWeatherByLocation (Location _location);
-
     public int loadWindDirection ();
-
     public double loadWindSpeed ();
-
     public double loadTemperature ();
+
 }
