a3f768c70cc0dc69e497ab5cd1cd81a830049ce9
jgschuma <47636951+jgschuma@users.noreply.github.com>
2019-12-09 18:15:06 -0500

    The comments for Ai

--- models/interpreters/BattleShipBotAi.java
+++ models/interpreters/BattleShipBotAi.java
@@ -43,6 +43,7 @@ public class BattleShipBotAi {
             opponent = BattleShipPlayer.AWAY;
         }
     }
+<<<<<<< Updated upstream
 
     public void takeShot () {
         xPos = (int)Math.random()*10+1;
@@ -56,6 +57,152 @@ public class BattleShipBotAi {
             else{
                 takeShot();
             }
+=======
+    /*
+    * @param _event: An event, either StartGameEvent, or ShipHitEvent
+    * Catch event listens for an event, it it's a startGameEvent, it will begin
+    * the AI cycle.
+    * If it's a ShipHitEvent, it checks if an enemy ship just got hit and 
+    * saves the nearby coordinates.
+    */
+    @Override
+    public void catchEvent (Object _event) {
+
+        if (_event instanceof StartGameEvent) {
+            this.AiLoop.setCycleCount(Timeline.INDEFINITE);
+            this.AiLoop.play();
+        }
+
+        if (_event instanceof ShipHitEvent) {
+            ShipHitEvent event = (ShipHitEvent)_event;
+            if (this.player.getPlayerTeam() != event.getPlayer()) {
+                int row = event.getRow();
+                int column = event.getColumn();
+                Coordinate curCoordinate;
+                List<Integer> plusMinus = Arrays.asList(1, -1);
+                for (int multiplicity : plusMinus) {
+                    curCoordinate = new Coordinate(row + multiplicity * 1, column + 0);
+                    if (BattleShipBoard.boardBoundaryCheck(curCoordinate)) {
+                        if (!this.coordinateInList(this.alreadyShot, curCoordinate)) {
+                            this.predictedShots.add(curCoordinate);
+                        }
+                    }
+                    curCoordinate = new Coordinate(row + 0, column - multiplicity * 1);
+                    if (BattleShipBoard.boardBoundaryCheck(curCoordinate)) {
+                        if (!this.coordinateInList(this.alreadyShot, curCoordinate)) {
+                            this.predictedShots.add(curCoordinate);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    /*
+    * This method begins the Ai loop so it will repeat these actions.
+    */
+    private void startAi() {
+        this.AiLoop.pause();
+    }
+    /*
+    * createPossibleShotArray populates the possible shots arrayList with all
+    * possible coorinates.
+    */
+    private void createPossibleShotArray () {
+        for (int x = 0; x < BattleShipBoard.BOARDSIZE; ++x) {
+            for (int y = 0; y < BattleShipBoard.BOARDSIZE; ++y) {
+                this.possibleShots.add(new Coordinate(x, y));
+            }
+        }
+    }
+    /*
+    * @param _shots: an arraylist
+    * getRandomPossibleShot randomly returns a coordinate from the arrayList 
+    * passed in.
+    */
+    private Coordinate getRandomPossibleShot (ArrayList<Coordinate> _shots) {
+        int randIndex = (int)(Math.random() * (_shots.size() - 1));
+        Coordinate randCoordinate = _shots.get(randIndex);
+        _shots.remove(randIndex);
+        return randCoordinate;
+    }
+    
+    /*
+    * makePredictedShot will check to see if there are any coordinates left in
+    * the predictedShots arrayList. If there is it will randomly fire at one of 
+    * them. Otherwise it will return false to indicate an empty arrayList.
+    */
+    private boolean makePredictedShot () {
+        if (!this.predictedShots.isEmpty()) {
+            Coordinate predictedCoordinate = this.getRandomPossibleShot(this.predictedShots);
+            this.takeShot(predictedCoordinate);
+            this.synchShotArrays();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    /*
+    * synchShotArrays will make sure that there is no overlap between
+    * predictedShots and PossibleShots.
+    */
+    private void synchShotArrays () {
+        this.possibleShots.forEach(possibleShot -> {
+            this.predictedShots.forEach(predictShot -> {
+                if (possibleShot == predictShot) {
+                    this.possibleShots.remove(possibleShot);
+                }
+            });
+        });
+    }
+    /*
+    * @param _list: The arraylist we're searchign through
+    * @param _coordinate: The coordinate we're looking for.
+    * coordinateInList checks to see if a given coordinate is in an arraylist 
+    * of coordinates.
+    */
+    private boolean coordinateInList (ArrayList<Coordinate> _list, Coordinate _coordinate) {
+        for (Coordinate curCoordinate : _list) {
+            if (curCoordinate.equals(_coordinate)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /*
+    * takeRandomShot will take a random shot from the possibleShots arrayList
+    * so long as possible shots is not empty
+    */
+    private void takeRandomShot () {
+        if(!this.possibleShots.isEmpty()) {
+        Coordinate randCoordinate = this.getRandomPossibleShot(this.possibleShots);
+        this.takeShot(randCoordinate);
+        }
+    }
+    
+    /*
+    * @param _coordinate: the coordinate to be fired at
+    * takeShot will fire at the enemy's board at the selected coordinate
+    */
+    private void takeShot (Coordinate _coordinate) {
+        this.lastShot = _coordinate;
+        this.player.setCurrentTarget(_coordinate);
+        BattleShipGame.getEventBus().throwEvent(new FireAwayEvent(this.opponent));
+        this.alreadyShot.add(_coordinate);
+    }
+    
+    /*
+    * takeTurn will see if it is the Ai's turn, if it is, it will take a shot.
+    */
+    private void takeTurn () {
+        if (this.player.isTurn()) {
+            if(!this.makePredictedShot()) {
+                this.takeRandomShot();
+            }
+        }
+>>>>>>> Stashed changes
     }
 
 }
